0ff085dcf39b9bafc51df17cefb0d63b4ec3d350
Vitalia Bilaniuk <vitaliiabilaniuk@gmail.com>
2019-02-17 21:17:25 +0200

    Webpack live reload. Architecture settings

---
+++ webpack.common.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {   app: './src/app.js'},
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+    title: 'My killer app'
+    })
+  ]
+}
