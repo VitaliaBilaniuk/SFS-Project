db9ba2913a9fb87d382f72d1d1da75ddde92293a
VitaliaBilaniuk <vitaliiabilaniuk@gmail.com>
2019-02-24 20:10:01 +0200

    Webpack settings changes

--- src/app.js
+++ src/app.js
@@ -5,9 +5,7 @@ button.textContent = 'Open chat'
 document.body.appendChild(button)
 
 button.onclick = () => {
-  import(/* webpackChunkName: "chat" */ "./chat").then(chat => {
-    chat.init()
-  })
+ console.log('working')
 }
 if (module.hot) {
     module.hot.accept()
