16f16502de74f2b84f1974ba37920ea27e210e9e
VitaliaBilaniuk <vitaliiabilaniuk@gmail.com>
2020-03-24 18:27:02 +0200

    Added switcher for compact version

--- src/components/tabs/Tab1.jsx
+++ src/components/tabs/Tab1.jsx
@@ -7,27 +7,57 @@ class Tab1 extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        copied: false
+        copied: false,
+        showCompactVersion: true
       };
     }
     
+    handleToggleChange = () => {
+      this.setState({
+        showCompactVersion: !this.state.showCompactVersion
+      });
+    }
+
     render() {
         const { form } = this.props;
         return (
         <div>
-            <div className="sfs-area-copied">
-                { form.name ? <p>{form.name}</p>
-                : <p>Johny Doe </p> }  
-                { form.position ? <p>{form.position}</p>
-                : <p>UI Engineer </p> } 
+          
+          <div className="sfs-area-copied">
+            Thanks
+            { form.name ? <p>{form.name}</p>
+            : <p>Johny Doe </p> }  
+            { form.position ? <p>{form.position}</p>
+            : <p>UI Engineer </p> } 
+            {
+              this.state.showCompactVersion?
+              <div>
                 <p>skype: { form.skype ? <span>{form.skype}</span>
                 : <span>vitaliia.bilaniuk </span> } </p>
-                <p>website: {form.website ? <span>{form.website}</span>
+                <p>web: {form.website ? <span>{form.website}</span>
                 : <span>perfectial.com</span> } </p>
                 <img src={PerfLogo} className="sfs-full-logo"/>
-            </div>
-            <div>
-            </div>
+              </div> :
+              <div>
+                <img src={PerfLogo} className="sfs-full-logo"/>
+                <p>w: {form.website ? <span>{form.website}</span>
+                : <span>perfectial.com</span> } </p>
+                <p>s: { form.skype ? <span>{form.skype}</span>
+                : <span>vitaliia.bilaniuk </span> } </p>
+              </div>
+            }  
+          </div>
+          
+          <div>
+            Signature style: {this.state.showCompactVersion? 'full': 'compact'}
+          </div>
+          <label className="switch">
+            <input 
+              type="checkbox"
+              onClick={this.handleToggleChange}
+            />
+            <span className="slider round"></span>
+          </label>
         </div>
         );
     }
